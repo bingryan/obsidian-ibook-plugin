8c0320ae5e569cb529e34cdf6cfaec5e4057b81b
bingryan <bingryan@outlook.com>
2023-03-04 10:56:04 +0800

    init

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+
+export interface IbookPluginSettings {
+	mySetting: string;
+}
+
+export const DEFAULT_SETTINGS: IbookPluginSettings = {
+	mySetting: 'default'
+};
+
---
+++ src/main.ts
@@ -0,0 +1,67 @@
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+import { IbookPluginSettings, DEFAULT_SETTINGS } from "./config";
+
+export default class IbookPlugin extends Plugin {
+	settings: IbookPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new IbookSettingTab(this.app, this));
+
+		// This adds an editor command that can perform some operation on the current editor instance
+		this.addCommand({
+			id: 'sample-editor-command',
+			name: 'Sample editor command',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				console.log(editor.getSelection());
+				editor.replaceSelection('Sample Editor Command');
+			}
+		});
+
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+
+class IbookSettingTab extends PluginSettingTab {
+	plugin: IbookPlugin;
+
+	constructor(app: App, plugin: IbookPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					console.log('Secret: ' + value);
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
