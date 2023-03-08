4d9027cfe84e965588452b430b61c6abc33bfef8
bingryan <bingryan@outlook.com>
2023-03-08 16:18:44 +0800

    fix transform error

--- src/export.ts
+++ src/export.ts
@@ -1,7 +1,9 @@
 import { getAllBookId, getBookById, getAnnotationBookId } from "@/api";
-import { IbookPluginSettings } from '@/config';
-import { Renderer } from '@/renderer';
-import IbookPlugin from '@/plugin'
+import { IbookPluginSettings } from "@/config";
+import { Renderer } from "@/renderer";
+import TurndownService from "turndown";
+
+import IbookPlugin from "@/plugin";
 import * as path from "path";
 import { tryCreateFolder } from "@/utils";
 
@@ -13,32 +15,46 @@ export class IBookExport implements IExport {
 	private settings: IbookPluginSettings;
 	private renderer: Renderer;
 	public plugin: IbookPlugin;
+
+	private turndownService: TurndownService;
 	constructor(settings: IbookPluginSettings, plugin: IbookPlugin) {
 		this.settings = settings;
 		this.renderer = new Renderer(this.settings);
 		this.plugin = plugin;
+		this.turndownService = new TurndownService();
 	}
 
 	/**
 	 * During the develop, the following problems will be found
-		 1. ZAEANNOTATION has data, but ZBKLIBRARYASSET has no book information	
+	1. ZAEANNOTATION has data, but ZBKLIBRARYASSET has no book information	
+	2. LibraryAsset.ZBOOKDESCRIPTION is HTML, but we want is markdown
 	
 	*/
 	async start() {
-		await tryCreateFolder(
-			this.plugin,
-			this.settings.output,
-		);
+		await tryCreateFolder(this.plugin, this.settings.output);
 
 		const getBookId = await getAllBookId();
 
 		if (getBookId.length > 0) {
 			for (let i = 0; i < getBookId.length; i++) {
-				const renderData = await this.getRenderDataById(getBookId[i].ZASSETID);
+				const renderData = await this.getRenderDataById(
+					getBookId[i].ZASSETID
+				);
 
-				if (renderData.annotation.length === 0 || renderData.library.ZTITLE === null) {
+				if (
+					renderData.annotation.length === 0 ||
+					renderData.library.ZTITLE === null
+				) {
 					continue;
 				}
+
+				/// ZBOOKDESCRIPTION: Convert HTML to markdown
+				if (renderData.library.ZBOOKDESCRIPTION !== null) {
+					renderData.library.ZBOOKDESCRIPTION = this.html3markdown(
+						renderData.library.ZBOOKDESCRIPTION
+					);
+				}
+
 				const content = this.renderer.render(renderData);
 				this.save(renderData.library.ZTITLE, content);
 			}
@@ -48,9 +64,8 @@ export class IBookExport implements IExport {
 	async getRenderDataById(bookId: string) {
 		const library = await getBookById(bookId);
 		const annotation = await getAnnotationBookId(bookId);
-		return { "library": library[0], "annotation": annotation }
+		return { library: library[0], annotation: annotation };
 	}
-
 
 	save(contentName: string, content: string) {
 		try {
@@ -63,6 +78,8 @@ export class IBookExport implements IExport {
 				throw error;
 			}
 		}
-
+	}
+	html3markdown(html: string) {
+		return this.turndownService.turndown(html);
 	}
 }
